93d22ed673ccb802cffe320d69a3c806c119d90f
AchahbarIlias <ilias.achahbar@student.ucll.be>
2021-11-26 08:20:40 +0100

    mysql check script

--- www/html/defaultpage/mysql_check.php
+++ www/html/defaultpage/mysql_check.php
@@ -1,14 +1,28 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-
+$username = "check";
+$password = "rDEetGxq82DCE";
+$dbname = "check";
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully <br>";
+
+
+$sql = "SELECT text FROM log ORDER BY date DESC LIMIT 1;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "text: " . $row["text"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
 ?>
